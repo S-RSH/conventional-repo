961728cb0837e7dbe5c76687d603e6500cd4a858
S-RSH <happer96@gmail.com>
2023-11-17 12:44:23 +0900

    Swap Error Merge on fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,11 @@
 # i -> print('hello') -> exc -> :wq + enter
 for i in range(1,16+1):
-    if i%3==0:
-        print('fizz')
-    elif i%5==0:
-        print('buzz')
-    else:
-        print(i)
+    if i%3==0 and i%5==0:
+        print('fizzbuzz')
+    else:    
+        if i%3==0:
+            print('fizz')
+        elif i%5==0:
+            print('buzz')
+        else:
+            print(i)
